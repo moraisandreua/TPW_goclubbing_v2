09ec26e180a4a6ce7e2cf2b4e2173be8ad8db492
Miguel Garcia Marques <61141327+mikemike198@users.noreply.github.com>
2021-06-08 22:54:37 +0100

    view to get all businesses and to get specific businesses done

--- backend/goclubbing/app/views.py
+++ backend/goclubbing/app/views.py
@@ -17,7 +17,28 @@ def get_all_businesses(request):
 
 
 @api_view(['GET'])
-def get_business_by_id(request):
+def get_business_by_fields(request):
+
     businesses = Business.objects.all()
+
+    if 'name' in request.GET:
+        businesses = businesses.filter(name__icontains=request.GET['name'])
+    if 'location' in request.GET:
+        businesses = businesses.filter(location__icontains=request.GET['location'])
+    if 'address' in request.GET:
+        businesses = businesses.filter(address__icontains=request.GET['address'])
+    if 'lat' in request.GET:
+        businesses = businesses.filter(lat__exact=request.GET['lat'])
+    if 'lon' in request.GET:
+        businesses = businesses.filter(lng__exact=request.GET['lng'])
+    if 'type' in request.GET:
+        businesses = businesses.filter(type__icontains=request.GET['type'])
+    if 'company_name' in request.GET:
+        businesses = businesses.filter(company_name__icontains=request.GET['company_name'])
+    if 'contact_email' in request.GET:
+        businesses = businesses.filter(contact_email__exact=request.GET['contact_email'])
+    if 'contact_phone' in request.GET:
+        businesses = businesses.filter(contact_phone__exact=request.GET['contact_phone'])
+
     serializer = BusinessSerializer(businesses, many=True)
     return Response(serializer.data)
