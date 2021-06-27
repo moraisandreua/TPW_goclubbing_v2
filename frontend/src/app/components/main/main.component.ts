6b2986d6ed67ee720c11f9a3fc4389defccd1955
moraisandreua <48691769+moraisandreua@users.noreply.github.com>
2021-06-27 12:24:09 +0100

    minor update to map

--- frontend/src/app/components/main/main.component.ts
+++ frontend/src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Directive } from '@angular/core';
+import { NgStyle } from '@angular/common';
 import * as L from 'leaflet';
 
 @Component({
@@ -7,29 +8,44 @@ import * as L from 'leaflet';
   styleUrls: ['./main.component.css']
 })
 
-
 export class MainComponent {
   title = 'Go Clubbing';
-
+  mapWidth:number;
+  mapHeight:number;
+  mapStyle:string;
   map: any;
+
+  constructor(){
+    this.mapWidth=window.innerWidth;
+    this.mapHeight=window.innerHeight;
+    this.mapStyle="width:1030; height:81";
+  }
 
   private initMap(): void {
     this.map = L.map('mymap', {
-      center: [39.8282, -98.5795],
+      center: [39.701104, -7.975786],
       zoom: 3
     });
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
-      minZoom: 3,
+      minZoom: 7,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
     tiles.addTo(this.map);
   }
 
+  public addMarker = (lat:number, long:number, descricao:string) => {
+    var marker = L.marker([lat, long]).addTo(this.map).bindPopup(descricao);
+  }
+
   ngAfterViewInit(): void {
     this.initMap();
+    //this.mapHeight=(document.getElementById("navbar") as HTMLElement).clientHeight;
+    //this.mapStyle="width: "+ this.mapWidth + "; height: " + this.mapHeight;
+    this.addMarker(41.111639, -8.647544,"Minha casa")
+    this.addMarker(41.096055, -8.656618,"Casa casa")
   }
 
 }
