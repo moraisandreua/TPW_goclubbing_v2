41a6bedb4c4cb9d98db3238a502adfaa75de7c48
moraisandreua <48691769+moraisandreua@users.noreply.github.com>
2021-06-30 11:57:30 +0100

    update signup
    - just a pause to merge another branch

--- frontend/src/app/services/authentication.service.ts
+++ frontend/src/app/services/authentication.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import {Observable} from "rxjs/internal/Observable";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {LoginToken} from "../LoginToken";
-import {Business} from "../Business";
-import {LoginComponent} from "../components/login/login.component";
+import {RegisterToken} from "../RegisterToken";
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type':'application/json'})
@@ -14,11 +13,15 @@ const httpOptions = {
 })
 export class AuthenticationService {
   private baseUrlLogin = "http://mike19.pythonanywhere.com/api/login/";
-
+  private baseUrlSignup = "http://mike19.pythonanywhere.com/api/create/business";
   constructor(private http: HttpClient) { }
 
   login(username:string, password:string):Observable<LoginToken>{
     const url = this.baseUrlLogin;
     return this.http.post<LoginToken>(url, {username:username, password:password})
+  }
+
+  signup(name:string, location:string, type:string, company_name:string, contact_email:string, contact_phone:string, username:string, password:string):Observable<RegisterToken>{
+    const url = this.baseUrlSignup;
   }
 }
