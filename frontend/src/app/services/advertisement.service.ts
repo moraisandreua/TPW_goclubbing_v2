fd4a42cf298f8ac8585f3f1a17a75645762c255c
Alexa <alexandracarvalho@ua.pt>
2021-06-30 11:35:29 +0100

    Updated business service and added advertisement service

---
+++ frontend/src/app/services/advertisement.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs/internal/Observable";
+import {Event} from "../Event";
+import {Ad} from "../Ad";
+
+const httpOptions = {
+  headers : new HttpHeaders({'Content-Type': 'application/json', 'Authentication':'1'})
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdvertisementService {
+  private baseURL = 'http://mike19.pythonanywhere.com/'
+  constructor(private http : HttpClient) { }
+
+  getAdvertisement(id : number) : Observable<Ad[]>{
+    const url = this.baseURL + 'api/advertisements/search?id=' + id;
+    return this.http.get<Ad[]>(url);
+  }
+
+  getAdvertisements() : Observable<Ad[]>{
+    const url = this.baseURL + 'api/advertisements/all';
+    return this.http.get<Ad[]>(url)
+  }
+
+  createAdvertisement(ad : Ad): Observable<any>{
+    const url = this.baseURL + 'api/create/advertisement';
+    return this.http.post(url, ad, httpOptions);
+  }
+
+  updateAdvertisement(ad: Ad): Observable<any>{
+    const url = this.baseURL + 'api/update/advertisement';
+    return this.http.put(url, ad, httpOptions);
+  }
+
+  deleteAdvertisement(ad: Ad): Observable<any>{
+    const url = this.baseURL + 'api/delete/advertisement/' + ad.id;
+    return this.http.delete<Event>(url, httpOptions);
+  }
+}
--- frontend/src/app/services/business.service.ts
+++ frontend/src/app/services/business.service.ts
@@ -2,25 +2,41 @@ import { Injectable } from '@angular/core';
 import {Observable} from "rxjs/internal/Observable";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Business} from "../Business";
+import {Event} from "../Event";
 
 const httpOptions = {
-  headers: new HttpHeaders({'Content-Type':'application/json'})
+  headers : new HttpHeaders({'Content-Type': 'application/json', 'Authentication':'1'})
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class BusinessService {
-  private baseUrl = "http://mike19.pythonanywhere.com/api/business/search";
+  private baseUrl = "http://mike19.pythonanywhere.com/";
   constructor(private http: HttpClient) { }
 
   getBusiness(id:number):Observable<Business[]>{
-    const url = this.baseUrl + "?id="+id;
+    const url = this.baseUrl + "api/business/search?id="+id;
     return this.http.get<Business[]>(url);
   }
 
   getBusinesses():Observable<Business[]>{
-    const url = this.baseUrl;
+    const url = this.baseUrl + 'api/business/all';
     return this.http.get<Business[]>(url);
+  }
+
+  createBusiness(business : Business): Observable<any>{
+    const url = this.baseUrl + 'api/create/business';
+    return this.http.post(url, business, httpOptions);
+  }
+
+  updateBusiness(business: Business): Observable<any>{
+    const url = this.baseUrl + 'api/update/business';
+    return this.http.put(url, business, httpOptions);
+  }
+
+  deleteEvent(event: Event): Observable<any>{
+    const url = this.baseUrl + 'api/delete/business/' + event.id;
+    return this.http.delete<Event>(url, httpOptions);
   }
 }
