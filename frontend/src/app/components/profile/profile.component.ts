0366d9c21ac1ccd94ce4a3fdb931cddf780b49db
Alexa <alexandracarvalho@ua.pt>
2021-07-01 12:08:59 +0100

    profile form now posts with profile picture in base64 - working

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -11,7 +11,7 @@ import {CookieService} from "ngx-cookie-service";
 })
 export class ProfileComponent implements OnInit {
   profile!: Business;
-
+  profileImageBase64!: string;
   thisBusiness: number;
 
   constructor(private router : Router, private cookieService : CookieService, private businessService : BusinessService) {
@@ -31,24 +31,33 @@ export class ProfileComponent implements OnInit {
     this.businessService.getBusiness(thisBusiness).subscribe(profile => this.profile = profile[0]);
   }
 
-  getBase64(file: any) : any{
-    console.log(file);
-    let reader = new FileReader();
-    if(file)
-      reader.readAsDataURL(file);
-    reader.onload = function () {
-      //me.modelvalue = reader.result;
-      console.log(reader.result);
-      return reader.result;
-    };
-    reader.onerror = function (error) {
-      console.log('Error: ', error);
-      return "null";
-    };
+  getBase64(fileInput: any) : any{
+    if (fileInput.target.files) {
+      if (fileInput.target.files[0]) {
+        // Size Filter Bytes
+        const max_size = 20971520;
+        const allowed_types = ['image/png', 'image/jpeg'];
+        const max_height = 15200;
+        const max_width = 25600;
+
+        if (fileInput.target.files[0].size > max_size) {
+          return false;
+        }
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          const image = new Image();
+          image.src = e.target.result;
+          image.onload = rs => {
+            const imgBase64Path = e.target.result;
+            this.profileImageBase64 = imgBase64Path.split(",")[1];
+          };
+        };
+      }
+    }
   }
 
-
   save(profile : Business) : void{
+    profile.profilePhoto = this.profileImageBase64;
     this.businessService.updateBusiness(profile).subscribe(answer => {
       console.log(answer);
       this.router.navigate(["/dashboard"]);
